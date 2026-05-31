01ac1155a5a524e166ba58c8f52eda15b33f58e5
six-two <git@six-two.dev>
2026-05-31 08:57:32 +0200

    test: Added 'both' mode to test script. Renamed it and removed old, no longer working one

---
+++ standalone-cli-test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# switch to this script's directory
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+
+if [[ $# -eq 1 ]] && [[ "$1" == both || "$1" == split ]]; then
+    MODE="$1"
+else
+    echo "[!] Usage: <MODE>"
+    echo "Where MODE can be one of:"
+    echo "- 'both': replace placeholders in one call"
+    echo "- 'split': separate invocations during markdown and html phase"
+    exit 1
+fi
+
+rm -r docs.bak
+cp -r docs docs.bak
+
+if [[ "$MODE" == split ]]; then
+    markdown-placeholder-standalone --docs docs.bak/ --placeholder-extra-js placeholder-extra.js --phase markdown
+    properdocs build -f properdocs-standalone-test.yml
+    markdown-placeholder-standalone --docs site/ --placeholder-extra-js placeholder-extra.js --phase html
+else
+    properdocs build -f properdocs-standalone-test.yml
+    markdown-placeholder-standalone --docs site/ --placeholder-extra-js placeholder-extra.js --phase both
+fi
+
+python3 -m http.server --directory site/
